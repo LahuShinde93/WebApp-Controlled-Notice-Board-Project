8d44c2021f65572ff2701411c517d96a7721cf5c
Lahu _Shinde@Wenable Inc <lahu.shinde@wenable.com>
2026-04-12 16:05:17 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/NoticeBoard/WebAppControlledNoticeBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.NoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppControlledNoticeBoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppControlledNoticeBoardProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NoticeBoard/WebAppControlledNoticeBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoticeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppControlledNoticeBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
